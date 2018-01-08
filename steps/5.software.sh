25f74afa94d5fe8c8394b767b0b76c6aec63c0ea
Bramus <bramus@bram.us>
2018-01-08 12:44:34 +0100

    ✨ Install postman

--- steps/5.software.sh
+++ steps/5.software.sh
@@ -345,6 +345,8 @@ brew cask install colorpicker-skalacolor
 brew cask install steam
 brew cask install xact
 
+brew cask install postman
+
 # Locking down to this version (no serial for later version)
 brew cask install https://raw.githubusercontent.com/grettir/homebrew-cask/36b240eeec68e993a928395d3afdcef1e32eb592/Casks/screenflow.rb
 
