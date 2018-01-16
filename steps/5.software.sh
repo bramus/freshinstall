fbc3bc6f69feba91ad6eacd6160ba6f6b276bbe6
Bramus <bramus@bram.us>
2018-01-16 21:58:01 +0100

    🔨 Tweak install of Android dev stuff
    - Install newer build tools
    - Actually install Android Studio
    - Fix PATH stuff in bash_profile
    - Configure a virtual device

--- steps/5.software.sh
+++ steps/5.software.sh
@@ -379,18 +379,22 @@ brew cask install docker
 ###############################################################################
 
 # @ref https://gist.github.com/agrcrobles/165ac477a9ee51198f4a870c723cd441
+# @ref https://gist.github.com/spilth/e7385e7f5153f76cca40a192be35f4ba
+
+touch ~/.android/repositories.cfg
 
 # Android Dev Tools
 brew cask install caskroom/versions/java8
 brew install ant
 brew install maven
 brew install gradle
+# brew install qt
 brew cask install android-sdk
 brew cask install android-ndk
 
 # SDK Components
-sdkmanager "platform-tools" "platforms;android-23"
-sdkmanager "build-tools;23.0.1"
+sdkmanager "platform-tools" "platforms;android-25" "extras;intel;Hardware_Accelerated_Execution_Manager" "build-tools;25.0.3" "system-images;android-25;google_apis;x86" "emulator"
+# echo y | …
 
 # HAXM
 if [ $(sw_vers -productVersion | cut -d. -f2) -lt 13 ]; then
@@ -400,23 +404,32 @@ else
 fi;
 
 # ENV Variables
-echo "export ANT_HOME=/usr/local/opt/ant" >> ~/.bash_profile
-echo "export MAVEN_HOME=/usr/local/opt/maven" >> ~/.bash_profile
-echo "export GRADLE_HOME=/usr/local/opt/gradle" >> ~/.bash_profile
-echo "export ANDROID_HOME=/usr/local/share/android-sdk" >> ~/.bash_profile
-echo "export ANDROID_NDK_HOME=/usr/local/share/android-ndk" >> ~/.bash_profile
-echo "export INTEL_HAXM_HOME=/usr/local/Caskroom/intel-haxm" >> ~/.bash_profile
+echo 'export ANT_HOME=/usr/local/opt/ant' >> ~/.bash_profile
+echo 'export MAVEN_HOME=/usr/local/opt/maven' >> ~/.bash_profile
+echo 'export GRADLE_HOME=/usr/local/opt/gradle' >> ~/.bash_profile
+echo 'export ANDROID_HOME=/usr/local/share/android-sdk' >> ~/.bash_profile
+echo 'export ANDROID_NDK_HOME=/usr/local/share/android-ndk' >> ~/.bash_profile
+echo 'export INTEL_HAXM_HOME=/usr/local/Caskroom/intel-haxm' >> ~/.bash_profile
 
-echo "export PATH=$ANT_HOME/bin:$PATH" >> ~/.bash_profile
-echo "export PATH=$MAVEN_HOME/bin:$PATH" >> ~/.bash_profile
-echo "export PATH=$GRADLE_HOME/bin:$PATH" >> ~/.bash_profile
-echo "export PATH=$ANDROID_HOME/tools:$PATH" >> ~/.bash_profile
-echo "export PATH=$ANDROID_HOME/platform-tools:$PATH" >> ~/.bash_profile
-echo "export PATH=$ANDROID_HOME/build-tools/23.0.1:$PATH" >> ~/.bash_profile
+echo 'export PATH="$ANT_HOME/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$MAVEN_HOME/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$GRADLE_HOME/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$ANDROID_HOME/tools:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$ANDROID_HOME/platform-tools:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$ANDROID_HOME/build-tools/25.0.3:$PATH"' >> ~/.bash_profile
 
 source ~/.bash_profile
 
-# @TODO: Configure Emulator?!
+# Android Studio itself
+brew cask install android-studio
+
+# Configure Emulator
+# @ref https://gist.github.com/Tanapruk/b05e97d68a5969b4402650094145e913
+# @ref https://wiki.genexus.com/commwiki/servlet/wiki?14462,Creating+an+Android+Virtual+Device,
+echo no | avdmanager create avd --name "Nexus_5X_API_25" --abi "google_apis/x86" --package "system-images;android-25;google_apis;x86" --device "Nexus 5"
+
+# Start it via `emulator -avd Nexus_5X_API_25`
+# Beware of the QT error though, @see https://www.bram.us/2017/05/12/launching-the-android-emulator-from-the-command-line/
 
 ###############################################################################
 # ALL DONE NOW!                                                               #
