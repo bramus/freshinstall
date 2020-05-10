c52c73781089e431aaf2254cb2f1982b40dd8e9f
Bramus <bramus@bram.us>
2020-05-10 07:01:25 +0200

    ✨ Install kap
    https://getkap.co/

--- steps/5.software.sh
+++ steps/5.software.sh
@@ -436,6 +436,7 @@ brew cask install postman
 
 # Locking down to this version (no serial for later version)
 brew cask install https://raw.githubusercontent.com/grettir/homebrew-cask/36b240eeec68e993a928395d3afdcef1e32eb592/Casks/screenflow.rb
+brew cask install kap
 
 brew cask install subsurface
 brew cask install quik
