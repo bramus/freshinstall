bramus/freshinstall

commit edd637274826a2e34103e153102ed49165dd3447

✨ Use SSH Multiplexing (see #25)

steps/2.ssh.sh
@@ -53,11 +53,7 @@ if [ "$config_file_present" == "yes" ]; then
 	echo -e "\033[32mOK\033[0m"
 	# @TODO: Check for “Host *: UseKeychain yes”
 else
-	touch $SSH_DIR/config
-	echo "Host *" >> $SSH_DIR/config
-	echo "	AddKeysToAgent yes" >> $SSH_DIR/config
-	echo "	UseKeychain yes" >> $SSH_DIR/config
-	echo "" >> $SSH_DIR/config
+	cp ./resources/apps/ssh/config $SSH_DIR/config
 	echo -e "\033[93mCREATED\033[0m"
 fi;
 

resources/apps/ssh/config
@@ -1,0 +1,17 @@
+Host *
+	AddKeysToAgent yes
+	UseKeychain yes
+
+	# Always use SSH2
+	Protocol 2
+
+	# SSH Multiplexing
+	ControlMaster auto
+	ControlPath ~/.ssh/control/%r@%h:%p
+	ControlPersist 1800
+
+	# Also this stuff
+	Compression yes
+	TCPKeepAlive yes
+	ServerAliveInterval 20
+	ServerAliveCountMax 10

commit 1f3b1f9335ddac2a211880c022ba208375b607d7

🚑 Make sure ~/.ssh/control folder exists
Fixup for edd6372, which introduced this issue when SSH'ing:

> unix_listener: cannot bind to path ~/.ssh/control/…: No such file or directory

steps/2.ssh.sh
@@ -53,6 +53,7 @@ if [ "$config_file_present" == "yes" ]; then
 	echo -e "\033[32mOK\033[0m"
 	# @TODO: Check for “Host *: UseKeychain yes”
 else
+	mkdir -p $SSH_DIR/control
 	cp ./resources/apps/ssh/config $SSH_DIR/config
 	echo -e "\033[93mCREATED\033[0m"
 fi;